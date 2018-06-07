7ed22966bacf29343d40bcdff63951c3963b5883
Rose Reyes <roseannlorinreyes@gmail.com>
2018-06-06 20:43:23 -0700

    removed selection sort

--- Sort.js
+++ Sort.js
@@ -1,19 +1,1 @@
 
-function sSort(arr){
-    for(var i =0; i < arr.length; i++){
-        let min = arr[i];
-        for (var j=i; j < arr.length; j++){
-            if(arr[j] < min){
-                min = arr[j];
-                minidx = j;
-            }
-        }
-        let temp = arr[i];
-        arr[i] = min;
-        arr[minidx] = temp;
-    }
-    return arr;
-}
-
-var z = sSort([89,4,9,100,3]);
-console.log(z);
