788e9aa36f03eb03556d13674012b43d2a52fca0
Rose Reyes <roseannlorinreyes@gmail.com>
2018-06-08 12:12:19 -0700

    added selectionSort

--- Sort.js
+++ Sort.js
@@ -13,8 +13,29 @@ function bubbleSort(arr) {
     }
     return arr;
 }
-var b = bubbleSort([33,55,4,19,5,7]);
-console.log(b);
+//var b = bubbleSort([33,55,4,19,5,7]);
+//console.log(b);
+
+
+function sSort(arr) {
+    var temp =0;
+    for(var x=0; x < arr.length; x++){
+        var currMin = x;
+        for(var y=1; y < arr.length; y++){
+            if(arr[y] < arr[currMin]){
+                currMin = y;
+            }
+            else {
+                temp = arr[y];
+                arr[y] = arr[currMin + 1];
+                arr[currMin + 1] = temp;
+            }
+        }
+    }
+   return arr; 
+}
+var c = sSort([3, 44, 38, 5, 47, 15]);
+console.log(c);
 
 
 
